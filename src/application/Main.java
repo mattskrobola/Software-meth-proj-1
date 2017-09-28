b1698ed7756fbfe3fbf53ec33b0bd9956bffb921
mattskrobola <mattskrobola@gmail.com>
2017-09-27 20:04:33 -0400

    test

--- src/application/Main.java
+++ src/application/Main.java
@@ -6,7 +6,7 @@ import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import view.LibraryController;
-
+//git commmerjtrkghdkjfhkdjfhgkdjhfgkjh
 
 public class Main extends Application {
 	
