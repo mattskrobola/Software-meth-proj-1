9ee2e796da922cc67b1be5708ac133d540bbad9c
kdreagle <kdreagle@gmail.com>
2017-09-27 20:43:15 -0400

    Dank

--- Song Library/src/application/SongLib.java
+++ Song Library/src/application/SongLib.java
@@ -20,7 +20,7 @@ public class SongLib extends Application {
 		LibraryController libraryController = loader.getController();
 		libraryController.start(primaryStage);
 		
-		Scene scene = new Scene(root,400,400);
+		Scene scene = new Scene(root,300,400);
 		// scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 		primaryStage.setScene(scene);
 		primaryStage.show();
