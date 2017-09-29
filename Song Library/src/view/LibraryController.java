439c73057dd858a95c97ae74b57e0d71d8b1558b
kdreagle <kdreagle@gmail.com>
2017-09-29 02:47:07 -0400

    Added confirmation and warning alerts

--- Song Library/src/view/LibraryController.java
+++ Song Library/src/view/LibraryController.java
@@ -2,6 +2,7 @@ package view;
 
 import java.io.*;
 import java.util.ArrayList;
+import java.util.Optional;
 import java.util.Scanner;
 
 import application.Song;
@@ -10,9 +11,14 @@ import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonBar.ButtonData;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.Dialog;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
 import javafx.stage.Stage;
 
 public class LibraryController {
@@ -90,7 +96,51 @@ public class LibraryController {
 			//TODO check if song exists
 			if(!exists(new Song(addSongName.getText(), addArtist.getText(), addAlbum.getText(), addYear.getText()))){
 				if (!addSongName.getText().isEmpty() && !addArtist.getText().isEmpty()) {
-					songs.add(new Song(addSongName.getText(), addArtist.getText(), addAlbum.getText(), addYear.getText()));
+					if (confirmAction(mainStage,"Are you sure you want to add this song?")) {
+						songs.add(new Song(addSongName.getText(), addArtist.getText(), addAlbum.getText(), addYear.getText()));
+						sortAndUpdate(songs);
+						try {
+							writeToFile(songs);
+						} catch (IOException e) {
+							//change this
+							e.printStackTrace();
+						}
+						listView.setItems(songList);
+						int tempIndex = findIndex(songs, addSongName.getText(), addArtist.getText());
+						listView.getSelectionModel().select(tempIndex);
+						clearTextFields();
+					}
+				}
+				
+			} else {
+				warning(mainStage, "The song that you're trying to add already exists!");
+			}
+			
+			// write list to file here
+		});
+		
+		editButton.setOnAction((event) -> {
+			
+			if(!exists(new Song(addSongName.getText(), addArtist.getText(), addAlbum.getText(), addYear.getText()))){
+			
+				int index = listView.getSelectionModel().getSelectedIndex();
+				
+				if (index >= 0 && confirmAction(mainStage,"Are you sure you want to edit this song?")) {
+					Song tempSong = songs.get(index);
+					
+					if(!addSongName.getText().isEmpty()) {
+						tempSong.setSongName(addSongName.getText());
+					}
+					if(!addArtist.getText().isEmpty()) {
+						tempSong.setArtist(addArtist.getText());
+					}
+					if(!addYear.getText().isEmpty()){
+						tempSong.setYear(addYear.getText());
+					}
+					if(!addAlbum.getText().isEmpty()) {
+						tempSong.setAlbum(addAlbum.getText());
+					}
+					songs.set(index,tempSong);
 					sortAndUpdate(songs);
 					try {
 						writeToFile(songs);
@@ -99,48 +149,14 @@ public class LibraryController {
 						e.printStackTrace();
 					}
 					listView.setItems(songList);
-					int tempIndex = findIndex(songs, addSongName.getText(), addArtist.getText());
-					listView.getSelectionModel().select(tempIndex);
 					clearTextFields();
+					listView.getSelectionModel().select(index);
 				}
 				
-			}
-			
-			// write list to file here
-		});
-		
-		editButton.setOnAction((event) -> {
-			
-			int index = listView.getSelectionModel().getSelectedIndex();
-			
-			if (index >= 0) {
-				Song tempSong = songs.get(index);
 				
-				if(!addSongName.getText().isEmpty()) {
-					tempSong.setSongName(addSongName.getText());
-				}
-				if(!addArtist.getText().isEmpty()) {
-					tempSong.setArtist(addArtist.getText());
-				}
-				if(!addYear.getText().isEmpty()){
-					tempSong.setYear(addYear.getText());
-				}
-				if(!addAlbum.getText().isEmpty()) {
-					tempSong.setAlbum(addAlbum.getText());
-				}
-				songs.set(index,tempSong);
-				sortAndUpdate(songs);
-				try {
-					writeToFile(songs);
-				} catch (IOException e) {
-					//change this
-					e.printStackTrace();
-				}
-				listView.setItems(songList);
-				clearTextFields();
-				listView.getSelectionModel().select(index);
+			} else {
+				warning(mainStage, "The song that you're trying to edit already exists somewhere else!");
 			}
-			
 			// write list to file here
 		});
 		
@@ -148,7 +164,7 @@ public class LibraryController {
 			
 			int index = listView.getSelectionModel().getSelectedIndex();
 			
-			if (!songList.isEmpty() && index >= 0) {
+			if (!songList.isEmpty() && index >= 0 && confirmAction(mainStage,"Are you sure you want to delete this song?")) {
 				int selectIndex = listView.getSelectionModel().getSelectedIndex();
 				songs.remove(index);
 				sortAndUpdate(songs);
@@ -244,6 +260,32 @@ public class LibraryController {
 		addArtist.clear();
 		addYear.clear();
 		addAlbum.clear();
+	}
+	
+	public void warning(Stage mainStage, String contentText) {
+		Alert warning = new Alert(AlertType.WARNING);
+		warning.initOwner(mainStage);
+		warning.setTitle("Oops!");
+		warning.setHeaderText(contentText);
+		warning.showAndWait();
+	}
+	
+	public boolean confirmAction(Stage mainStage, String contentText) {
+		Alert confirmation = new Alert(AlertType.CONFIRMATION);
+		confirmation.initOwner(mainStage);
+		confirmation.setContentText(contentText);
+		
+		ButtonType yesButton = new ButtonType("Yes");
+		ButtonType cancelButton = new ButtonType("Cancel", ButtonData.CANCEL_CLOSE);
+		
+		confirmation.getButtonTypes().setAll(yesButton, cancelButton);
+		
+		Optional<ButtonType> result = confirmation.showAndWait();
+		if (result.get() == yesButton)  {
+			return true;
+		} else {
+			return false;
+		}
 	}
 
 	
